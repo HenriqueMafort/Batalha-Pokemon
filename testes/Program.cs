34efcf7e8e2c44b62fefdb3d2c23c07c68d6fb9e
HenriqueMafort <hmafort@gmail.com>
2022-11-24 10:33:13 -0300

    Melhorias no codigo

--- testes/Program.cs
+++ testes/Program.cs
@@ -2,11 +2,11 @@
 
 string pokemon, jogador_a;
 int vida_mewtwo = 250, ataque_mewtwo = 40;
-//vida_jogador = 250, ataque_jogador = 50, cura_jogador = 50
 char certeza = 'n', opcao, opcao_turno, restart = 's';
 
 Pokemon dadospokemon = new Pokemon();
 Pokemon objpokemon = new Pokemon();
+Pokemon objescolhaPokemon = new Pokemon();
 dadospokemon = objpokemon;
 
 
@@ -23,108 +23,110 @@ while (restart == 's' || restart == 'S')
 {
 
 
-    while (certeza == 'n' || certeza == 'N')
+
+
+    Console.WriteLine(jogador_a + " escolha seu pokemon");
+    do
     {
-        Console.WriteLine(jogador_a + " escolha seu pokemon");
         Console.WriteLine("A - Charizard");
         Console.WriteLine("B - Squirtle");
         Console.WriteLine("C - Pikachu");
-
         opcao = char.Parse(Console.ReadLine());
         Console.WriteLine("Digite corretamente os valores: ");
         Console.Clear();
 
-        switch (opcao)
-        {
-            case 'A':
-            case 'a':
-                objpokemon.pokemon = "Charizard";
-                objpokemon.ataque_jogador = 45;
-                objpokemon.vida_jogador = 190;
-                objpokemon.cura_jogador = 50;
+            switch (opcao)
+            {
+                case 'A':
+                case 'a':
+                    objpokemon = new Pokemon("Charizard", 190, 45, 50);
+                    Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
+                    certeza = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                    if (certeza != 's' || certeza != 'S')
+                    {
+                        Console.WriteLine("A opção " + certeza + " não é valida, Digite uma opção valida: ");
+                    }
+                    break;
+                    break;
+                case 'B':
+                case 'b':
 
+                    objpokemon = new Pokemon("Squirtle", 170, 70, 35);
+                    Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
+                    certeza = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                    if (certeza != 's' || certeza != 'S')
+                    {
+                        Console.WriteLine("A opção " + certeza + " não é valida, Digite uma opção valida: ");
+                    }
+                    break;
+                    break;
 
-                Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
-                certeza = char.Parse(Console.ReadLine());
+                case 'C':
+                case 'c':
+                    objpokemon = new Pokemon("Pikachu", 120, 90, 50);
+                    Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
+                    certeza = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                    if (certeza != 's' || certeza != 'S')
+                    {
+                        Console.WriteLine("A opção " + certeza + " não é valida, Digite uma opção valida: ");
+                    }
+                    break;
+            }
+        } while (certeza != 's' || certeza == 'S');
 
-                break;
-            case 'B':
-            case 'b':
-                objpokemon.pokemon = "Squirtle";
-                objpokemon.vida_jogador = 170;
-                objpokemon.cura_jogador = 70;
-                objpokemon.ataque_jogador = 35;
-
-                Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
-                certeza = char.Parse(Console.ReadLine());
-                break;
-
-            case 'C':
-            case 'c':
-
-                objpokemon.pokemon = "Pikachu";
-                objpokemon.vida_jogador = 120;
-                objpokemon.ataque_jogador = 90;
-                objpokemon.cura_jogador = 50;
-
-                Console.WriteLine(objpokemon.getdadospokemon() + objpokemon.getConfirmacaoPokemon());
-                certeza = char.Parse(Console.ReadLine());
-                break;
-        }
-    }
-
-
-    Console.WriteLine("Boa, agora é hora de batalhar : ");
-    Console.WriteLine("MeWtwo");
-    Console.WriteLine("Vida " + vida_mewtwo);
-    Console.WriteLine("Ataque " + ataque_mewtwo);
-
-    Console.Clear();
-
-    while (dadospokemon.vida_jogador > 0 && vida_mewtwo > 0)
-    {
-
-        Console.WriteLine("você possui " + dadospokemon.vida_jogador + " de vida");
-        Console.WriteLine("oque você deseja fazer ? ");
-        Console.WriteLine("A - Atacar");
-        Console.WriteLine("B - Curar");
-        opcao_turno = char.Parse(Console.ReadLine());
-
-        switch (opcao_turno)
-        {
-            case 'a':
-            case 'A':
-                vida_mewtwo = vida_mewtwo - dadospokemon.ataque_jogador;
-                Console.WriteLine("Agora atacou o MeWtwo e causou " + dadospokemon.ataque_jogador + " de dano");
-                Console.WriteLine("MeWtwo está com " + vida_mewtwo + " de vida: ");
-                break;
-
-            case 'b':
-            case 'B':
-                dadospokemon.vida_jogador = dadospokemon.vida_jogador + dadospokemon.cura_jogador;
-                Console.WriteLine("Seu pokemon se curou e está com " + dadospokemon.vida_jogador + " de vida");
-                break;
-        }
-
+        Console.WriteLine("Boa, agora é hora de batalhar : ");
+        Console.WriteLine("MeWtwo");
+        Console.WriteLine("Vida " + vida_mewtwo);
+        Console.WriteLine("Ataque " + ataque_mewtwo);
         Console.Clear();
 
-
-        if (vida_mewtwo >= 0)
+        while (objpokemon.vida_jogador > 0 && vida_mewtwo > 0)
         {
-            dadospokemon.vida_jogador = dadospokemon.vida_jogador - ataque_mewtwo;
-            Console.WriteLine("Mewtwo te atacou e causou " + ataque_mewtwo + " de dano");
 
+            Console.WriteLine("você possui " + objpokemon.vida_jogador + " de vida");
+            Console.WriteLine("oque você deseja fazer ? ");
+            Console.WriteLine("A - Atacar");
+            Console.WriteLine("B - Curar");
+            opcao_turno = char.Parse(Console.ReadLine());
 
-        }
-        if (dadospokemon.vida_jogador <= 0)
-        {
+            switch (opcao_turno)
+            {
+                case 'a':
+                case 'A':
+                    vida_mewtwo = vida_mewtwo - objpokemon.ataque_jogador;
+                    Console.WriteLine("Agora atacou o MeWtwo e causou " + objpokemon.ataque_jogador + " de dano");
+                    Console.WriteLine("MeWtwo está com " + vida_mewtwo + " de vida: ");
+                    break;
+
+                case 'b':
+                case 'B':
+                    objpokemon.vida_jogador = objpokemon.vida_jogador + objpokemon.cura_jogador;
+                    Console.WriteLine("Seu pokemon se curou e está com " + objpokemon.vida_jogador + " de vida");
+                    break;
+            }
+
             Console.Clear();
-            Console.WriteLine("acabou o jogo");
-        }
-        else if (dadospokemon.vida_jogador > vida_mewtwo)
-        {
-            Console.WriteLine("parabens você ganhou");
-        }
-    }
 
-}
+            if (vida_mewtwo >= 0)
+            {
+                objpokemon.vida_jogador = objpokemon.vida_jogador - ataque_mewtwo;
+                Console.WriteLine("Mewtwo te atacou e causou " + ataque_mewtwo + " de dano");
+                Console.WriteLine("A vida do Mewtwo é de " + vida_mewtwo);
+            }
+
+            if (objpokemon.vida_jogador <= 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Você perdeu: " + "\n\r" + "acabou o jogo");
+            }
+            else if (objpokemon.vida_jogador > 0 && vida_mewtwo <= 0)
+            {
+                Console.WriteLine("parabens você ganhou");
+            }
+        }
+        Console.WriteLine("Deseja jogar novamente ? ");
+        restart = char.Parse(Console.ReadLine());
+    }
--- testes/pokemon.cs
+++ testes/pokemon.cs
@@ -4,20 +4,34 @@ using System;
 
 public class Pokemon
 {
+
     public string pokemon;
     public int vida_jogador;
     public int ataque_jogador;
     public int cura_jogador;
+    
+    public Pokemon()
+    {
 
+    }
+    public Pokemon(string pokemon, int vida_jogador, int cura_jogador, int ataque_jogador)
+    {
+        this.ataque_jogador = ataque_jogador;
+        this.pokemon = pokemon;
+        this.vida_jogador = vida_jogador;
+        this.cura_jogador = cura_jogador;
+    }
 
     public string getdadospokemon()
     {
-        return "nome " + pokemon + "\r\n" + "Ataque " + ataque_jogador + "\r\n" + "Cura " + cura_jogador +  "\r\n";
+        return "nome: " + pokemon + "\r\n" + "Ataque: " + ataque_jogador + "\r\n" + "Cura: " + cura_jogador +  "\r\n";
     }
 
     public string getConfirmacaoPokemon()
     {
         return "Você tem certeza da sua opnião?" + "\r\n" + "Digite 'S' ou 'N'";
     }
+
+ 
 }
 
